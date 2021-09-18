434bc0382a44900014708fe2451fe2b2c4c09125
Mahadev <Mahadev.clever@gmail.com>
2021-09-18 19:03:31 +0530

    List model added

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,9 @@
+from core import models
 from django.contrib import admin
+from .models import List
 
-# Register your models here.
+
+class ListAdmin(admin.ModelAdmin):
+    """List Admin Definition"""
+    pass
+admin.site.register(List,ListAdmin)
