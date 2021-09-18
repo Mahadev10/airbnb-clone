dbbfa1304c874f323622a853bd8a0725f7232484
Mahadev <Mahadev.clever@gmail.com>
2021-09-18 07:22:17 +0530

    Review model added

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    """Review Admin Definition"""
+
+    pass
+
+
+admin.site.register(models.Review, ReviewAdmin)
